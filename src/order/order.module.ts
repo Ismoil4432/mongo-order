bdcd4b834bf154d6e3108b790e8863d17f2f8a77
Ismoil4432 <ismoil4432@gmail.com>
2023-03-24 14:32:51 +0500

    Mongodb

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,10 +3,12 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    JwtModule.register({}),
   ],
   controllers: [OrderController],
   providers: [OrderService],
